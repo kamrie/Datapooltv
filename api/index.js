e03941656c78aec44dbf7c291fae9cf0d3338e89
kamrie <ifedikeze@gmail.com>
2024-12-01 23:13:28 +0100

    create

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from 'express';   // "type":"module" was set in package.json so as to be able to use import else we'd use require. 
+// import bcryptjs from 'bcryptjs';
+
+
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("listening  3000!!!")
+})
