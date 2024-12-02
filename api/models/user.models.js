3ae02d713f059f607c4d2dec6d5f8108e51d887d
kamrie <ifedikeze@gmail.com>
2024-12-02 14:27:49 +0100

    created a database user model

---
+++ api/models/user.models.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+   username: {
+        type: String,
+        required: true,
+        unique: true, //no one should have a username similar to others, everyone will have a different username.
+        trim: true,
+
+    },
+   fullname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+ email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+ },
+ phone: {
+     type: String,
+     required: true,
+     match: /^(\+\d{1,3})?\d{10,15}$/,
+     trim: true,
+     },
+ referralUsername: {
+    type: String,
+    trim: true,
+    default: null, // Optional field
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8, 
+ },
+ address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  termsAccepted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+},  { timestamps: true } )  // Adds createdAt and updatedAt fields automatically
+
+
+
+const User  = mongoose.model('User', userSchema);
+
+export default User;
